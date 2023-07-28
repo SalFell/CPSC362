a3b77fcee6ac0400856e6c274c99d997f9580ce2
waterbearsinspace <21986797+waterbearsinspace@users.noreply.github.com>
2023-07-28 07:42:55 -0700

    Allow for chart "refresh"
    The chart is "updated" but the data isn't, will have to fix, but any
    changes are read between page refreshes after clearing cache.

--- scripts/create-chart.js
+++ scripts/create-chart.js
@@ -1,22 +1,25 @@
 
 import { readJSON } from "./read-json.js";
 
-// Canvas on which chart is created
-const ctx = document.getElementById('myChart');
-
-// Read JSON data
-const data = await readJSON();
-
 // Change button to say "Update Chart"
 function changeText() {
     var button = document.getElementById("chart-button");
     if (button.innerHTML == 'Create Chart') button.innerHTML = 'Update Chart';
 }
 
+const data = await readJSON();
+
 // Create chart using an array
-export function createChart() {
-    const myChart = new Chart(
-        ctx,
+export async function createChart() {
+    let chartStatus = Chart.getChart('myChart');
+    if (chartStatus != undefined) {
+        chartStatus.destroy();
+    }
+
+    var chartCanvas = document.getElementById('myChart');
+
+    const chartInstance = new Chart(
+        chartCanvas,
         {
         type: 'line',
         data: {
@@ -38,8 +41,4 @@ export function createChart() {
     );
 
     changeText();
-    
-    return myChart;
 }
-
-
--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 // Import Functions
 import { createChart } from "./create-chart.js";
-import { updateChart } from "./update-chart.js";
-
 
 // On Click Functions
+// Use button text as state
+var created = false;
+// Create chart, changes button to call updateChart() after first click
 document.getElementById("chart-button").onclick = function() {createChart()};
-//document.getElementById("update-chart").onclick = function() {updateChart()};
