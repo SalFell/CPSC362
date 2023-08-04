1a83583857a2a382c22b5aa511a16ca935681225
Mike Ball <michaelball357@gmail.com>
2023-08-04 16:13:40 -0700

    modification to make reading easier

--- server-side/Strategy_Pattern.js
+++ server-side/Strategy_Pattern.js
@@ -13,8 +13,9 @@ import { readHistoricalDataFile } from './DataFileFunctions.js';
 const historicalData = readHistoricalDataFile();
 
 const context = new Context(new MACO());
+const result = context.executeStrategy(historicalData);
 //console.log('MACO:' + context.executeStrategy(historicalData));
 //console.log('------------------------------------------------');
-writeTradeDataFile(context.executeStrategy(historicalData), 'MACO');
+writeTradeDataFile(result, 'MACO');
 //context.strategy = new BB();
 //console.log('BB:' + context.executeStrategy('data/historical_data.json'));
