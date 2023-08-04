5f2134ae0caab099fb292062b9adfd6ebf664df0
Salvador Felipe <salvadorfelipe77@hotmail.com>
2023-08-03 19:49:07 -0700

    trying to fix revert

--- server/index.js
+++ server/index.js
@@ -1,63 +1,31 @@
-const MACOmain = require('./MACO-Gabe');
-const BBmain = require('./BB');
-//const { BackTestResults } = require('../client/src/BackTestResults');
-const processData = require('./processData');
-const express = require('express');
-const axios = require('axios');
-const fs = require('fs');
-const cors = require('cors');
+// server/index.js
+
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-const port = process.env.PORT || 3000; // You can choose any available port number
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // CORS middleware
-app.use(cors({ origin: '*' }));
+app.use(cors({origin:'*'}));
 
-app.use(function(req, res, next) {
-res.header('Access-Control-Allow-Origin', '*');
-res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-next();
-})
-
-app.use(express.json());
-
-// Endpoint to proxy the Yahoo Finance API request
-app.get('/yahoo-finance/:symbol', async (req, res) => {
-  const symbol = req.params.symbol;
-  const { period1, period2, interval, events, includeAdjustedClose } = req.query;
-
-  try {
-    const response = await axios.get(
-      `http://query1.finance.yahoo.com/v7/finance/chart/${symbol}?period1=${period1}&period2=${period2}&interval=${interval}&events=${events}&includeAdjustedClose=${includeAdjustedClose}`
-    );
-    const data = response.data;
-    const historicalData = processData(data);
-
-    if (historicalData.length > 0) {
-      // Save data to the server
-      const fileName = `./data/${symbol}_historical_data.json`;
-      fs.writeFileSync(fileName, JSON.stringify(historicalData, null, 2), 'utf8');
-
-      // Backtest
-      const MACO = MACOmain(fileName);
-      // console.log(MACO);
-      const BB = BBmain(fileName);
-      // console.log(BB);
-      //BackTestResults(MACO);
-      console.log(`Data for ${symbol} downloaded successfully to ${fileName}`);
-
-      res.json(historicalData);
-    } else {
-      res.status(404).json({ error: 'No data received from Yahoo Finance API.' });
-    }
-  } catch (error) {
-    console.error('Error occurred during API request:', error.message);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json({ message: "Hello from server!" });
 });
 
-// Start the proxy server
-app.listen(port, () => {
-  console.log(`Proxy server is running on port ${port}`);
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
---
+++ server/proxy.js
@@ -0,0 +1,63 @@
+const MACOmain = require('./MACO-Gabe');
+const BBmain = require('./BB');
+//const { BackTestResults } = require('../client/src/BackTestResults');
+const processData = require('./processData');
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000; // You can choose any available port number
+
+
+// CORS middleware
+app.use(cors({ origin: '*' }));
+
+app.use(function(req, res, next) {
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+next();
+})
+
+app.use(express.json());
+
+// Endpoint to proxy the Yahoo Finance API request
+app.get('/yahoo-finance/:symbol', async (req, res) => {
+  const symbol = req.params.symbol;
+  const { period1, period2, interval, events, includeAdjustedClose } = req.query;
+
+  try {
+    const response = await axios.get(
+      `http://query1.finance.yahoo.com/v7/finance/chart/${symbol}?period1=${period1}&period2=${period2}&interval=${interval}&events=${events}&includeAdjustedClose=${includeAdjustedClose}`
+    );
+    const data = response.data;
+    const historicalData = processData(data);
+
+    if (historicalData.length > 0) {
+      // Save data to the server
+      const fileName = `./data/${symbol}_historical_data.json`;
+      fs.writeFileSync(fileName, JSON.stringify(historicalData, null, 2), 'utf8');
+
+      // Backtest
+      const MACO = MACOmain(fileName);
+      // console.log(MACO);
+      const BB = BBmain(fileName);
+      // console.log(BB);
+      //BackTestResults(MACO);
+      console.log(`Data for ${symbol} downloaded successfully to ${fileName}`);
+
+      res.json(historicalData);
+    } else {
+      res.status(404).json({ error: 'No data received from Yahoo Finance API.' });
+    }
+  } catch (error) {
+    console.error('Error occurred during API request:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Start the proxy server
+app.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
+});
